55202a5731fd4873ff05411fd894f1e65a58fc44
kruglay <kruglay@mail.ru>
2017-01-12 23:49:14 +0300

    add map, photo gallery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,21 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require map
 //= require_tree .
+
+// Код, который цепляет плагин lightbox к объектам с аттрибутом data-toggle="lightbox"
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+// // Скрываем форму добавления фотографии и показываем её по клику на ссылку
+// $(function(){
+//     $('.event-photo-form').hide();
+//
+//     $('.event-photo-form-link').click(function(){
+//         $('.event-photo-form').slideToggle(300);
+//         return false;
+//     });
+// });
